ea708d21ed61fbbcd39e2d63d7a64ee6e3a2b0f3
harunauchida <sayos8891@gmail.com>
2025-10-18 13:00:35 +0900

    新規登録画面エラー文言表示、戻るボタン追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,13 +4,14 @@ class UsersController < ApplicationController
     end
 
     def create
-        @user = User.new(user_params)
-        if @user.save
-            session[:user_id] = @user.id
-            redirect_to root_path, notice: "登録が完了しました！"
-        else
-            render :new
-        end
+      @user = User.new(user_params)
+      if @user.save
+        session[:user_id] = @user.id
+        redirect_to root_path, notice: "登録が完了しました！"
+      else
+        flash.now[:alert] = "登録に失敗しました"
+        render :new
+      end
     end
 
     def show; 
