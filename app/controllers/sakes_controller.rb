fc2cbb2e8112e8455d12e7ac75ee412a6496da49
harunauchida <sayos8891@gmail.com>
2025-10-19 15:47:05 +0900

    日本酒一覧に検索機能追加・カードの表示修正

--- app/controllers/sakes_controller.rb
+++ app/controllers/sakes_controller.rb
@@ -1,11 +1,29 @@
-class SakesController < ApplicationController
+class SakesController < ApplicationController  
   def index
-    @sweet_sakes = Sake.where(flavor_type: "sweet")   # DBでは英語で登録している場合
+    # トップページ用
+    @sweet_sakes  = Sake.where(flavor_type: "sweet")
     @medium_sakes = Sake.where(flavor_type: "medium")
-    @dry_sakes   = Sake.where(flavor_type: "dry")
+    @dry_sakes    = Sake.where(flavor_type: "dry")
 
-    # 日本酒一覧ページ用に全件取得
-    @sakes = Sake.all.order(:name)
+    # 日本酒一覧ページ用の検索処理
+    if params[:q].present?
+      keyword = params[:q]
+
+      # 日本語→英語 flavor_type の変換対応
+      flavor_map = {
+        "甘口" => "sweet",
+        "中口" => "medium",
+        "辛口" => "dry"
+      }
+      mapped_keyword = flavor_map[keyword] || keyword
+
+      @sakes = Sake.where(
+        "name LIKE :keyword OR flavor_type LIKE :keyword",
+        keyword: "%#{mapped_keyword}%"
+      ).order(:name)
+    else
+      @sakes = Sake.all.order(:name)
+    end
   end
 
   def show
