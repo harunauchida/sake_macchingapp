1057d15a4af2e6bfebf7aa92eaa67a6079dd9f39
harunauchida <sayos8891@gmail.com>
2025-10-24 08:40:46 +0900

    トップページのレイアウトの変更、おすすめの日本酒カード表示数3枚に変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController 
   def index
-    @all_sakes = Sake.limit(6)
+    @all_sakes = Sake.limit(3)
     Rails.logger.debug @all_sakes.map { |s| { name: s.name, image_path: s.image_path } }
   end
 end
