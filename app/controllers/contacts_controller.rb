1524eb94382ad53d8e2488b7a179a38799db97c1
harunauchida <sayos8891@gmail.com>
2025-11-03 15:47:15 +0900

    お問い合わせフォームの送信エラー修正

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,25 +1,20 @@
 class ContactsController < ApplicationController
-# 新規問い合わせフォーム
     def new
         @contact = Contact.new
     end
 
-# 送信処理
     def create
         @contact = Contact.new(contact_params)
-        if @contact.save
-# 成功時はトップページにリダイレクト
-        redirect_to root_path, notice: "お問い合わせを送信しました。"
+        if @contact.valid?   # save → valid? に変更
+            redirect_to root_path, notice: "お問い合わせを送信しました。"
         else
-# バリデーションエラー時はフォームを再表示
             flash.now[:alert] = "入力に問題があります。"
             render :new, status: :unprocessable_entity
         end
     end
 
-private
+    private
 
-# Strong Parameters
     def contact_params
         params.require(:contact).permit(:name, :email, :message)
     end
