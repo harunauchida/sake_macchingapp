9b5c56d7ff9de11510f71065c796db0944b59fc7
harunauchida <sayos8891@gmail.com>
2025-11-03 11:24:11 +0900

    日本酒タイプエラーの修正・トップページの修正

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,6 @@
 class HomeController < ApplicationController  
   def index
-    @sweet_sakes  = Sake.where(flavor_type: 'sweet').limit(3)
-    @medium_sakes = Sake.where(flavor_type: 'medium').limit(3)
-    @dry_sakes    = Sake.where(flavor_type: 'dry').limit(3)
-
     @all_sakes = Sake.limit(3)
-
-    Rails.logger.debug "Sweet: #{@sweet_sakes.map { |s| s.name }}"
-    Rails.logger.debug "Medium: #{@medium_sakes.map { |s| s.name }}"
-    Rails.logger.debug "Dry: #{@dry_sakes.map { |s| s.name }}"
+    Rails.logger.debug @all_sakes.map { |s| { name: s.name, image_path: s.image_path } }
   end
 end
