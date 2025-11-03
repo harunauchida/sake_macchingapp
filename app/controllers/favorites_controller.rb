cd764bfcc18b3bda517d1e9dbe3d89c57958158b
harunauchida <sayos8891@gmail.com>
2025-11-03 12:46:02 +0900

    気になるボタンのエラーを修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,10 +2,9 @@ class FavoritesController < ApplicationController
   before_action :require_login
   before_action :set_sake, only: [:create, :destroy]
 
-  # マイページ用：自分の気になる一覧
+  # 気になる一覧（マイページ用）
   def index
     @user = User.find(params[:user_id])
-    # マイページ以外はアクセス不可
     redirect_to root_path, alert: "権限がありません" unless @user == current_user
 
     @favorite_sakes = @user.favorite_sakes.includes(image_attachment: :blob, reviews: :user)
@@ -13,24 +12,30 @@ class FavoritesController < ApplicationController
 
   # 気になる追加
   def create
-    favorite = current_user.favorites.new(sake: @sake)
-    if favorite.save
-      redirect_to sakes_path, notice: "気になるに追加しました"
-    else
-      redirect_to sakes_path, alert: "すでに追加済みです"
+    @favorite = current_user.favorites.new(sake: @sake)
+    respond_to do |format|
+      if @favorite.save
+        format.html { redirect_back fallback_location: root_path, notice: "気になるに追加しました" }
+        format.turbo_stream
+      else
+        format.html { redirect_back fallback_location: root_path, alert: "すでに追加済みです" }
+        format.turbo_stream
+      end
     end
   end
 
   # 気になる解除
   def destroy
-    favorite = current_user.favorites.find_by(sake: @sake)
-    if favorite
-      favorite.destroy
-      notice = "気になるを解除しました"
-    else
-      notice = "お気に入りが見つかりませんでした"
+    @favorite = current_user.favorites.find_by(sake: @sake)
+    respond_to do |format|
+      if @favorite&.destroy
+        format.html { redirect_back fallback_location: root_path, notice: "気になるを解除しました" }
+        format.turbo_stream
+      else
+        format.html { redirect_back fallback_location: root_path, alert: "お気に入りが見つかりませんでした" }
+        format.turbo_stream
+      end
     end
-    redirect_to sakes_path, notice: notice
   end
 
   private
